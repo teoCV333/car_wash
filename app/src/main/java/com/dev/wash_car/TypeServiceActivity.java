1fb5650de968584b29df14f44ab591b9aaac0087
teoCV333 <teo333>
2023-11-03 21:48:15 -0500

    my service in progress

--- app/src/main/java/com/dev/wash_car/TypeServiceActivity.java
+++ app/src/main/java/com/dev/wash_car/TypeServiceActivity.java
@@ -2,13 +2,36 @@ package com.dev.wash_car;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class TypeServiceActivity extends AppCompatActivity {
+
+    Button btnSimple, btnNormal, btnEspecial, btnPremium, btnBack, btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_type_service);
+
+        btnSimple = findViewById(R.id.btnSimple);
+        btnNormal = findViewById(R.id.btnNormal);
+        btnEspecial = findViewById(R.id.btnEspecial);
+        btnPremium = findViewById(R.id.btnPremium);
+        btnBack = findViewById(R.id.btnBack);
+        btnSave = findViewById(R.id.btnSave);
     }
+
+    public void back(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void save(View view) {
+        Toast.makeText(TypeServiceActivity.this, "Se guardó tu selección", Toast.LENGTH_SHORT).show();
+    }
+
 }
